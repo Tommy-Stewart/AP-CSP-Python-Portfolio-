5e5cf24f08788d09b08932e4b72207a54b70c039
Tommy-Stewart <sthomas134@cps.edu>
2026-05-28 08:15:18 -0500

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,171 @@
+
+
+import random
+name = "poliwag"
+level = 1
+day = 1
+
+def level_1():
+    print((r" _..__                                    ___\n"))
+    print((r"/     `._                          ,--\"\"\"\"   `\"-.\n"))
+    print(("|         `.                    _.'\"\"/`.        |/`-.\n"))
+    print(("|           `.                ,+ `..' | |       |'...+.\n"))
+    print(("|     \\       \\              / /\\____,' '    __ `.`._,\".\n"))
+    print(("|      ..      \\           ,'  \\      .' ,-\"'  `. `.._,'`.\n"))
+    print(("|       .`.     \\         /     `-..-'  .  _.... |._      \\\n"))
+    print(("'        ' `     \\       /          ,-\"\"`.____...'  `.     \\\n"))
+    print((r"`        `.`.    \\     '         ,'   _,--------.`.  `.    L\n"))
+    print((r" `         ` `.   \\   j         /  .,' ,\"_.....`.`.`   \\   |\n"))
+    print((r"  `.        `. `.  \\  |        / ,'/ .','..... `.\\ \\|   .  |\n"))
+    print((r"    `.        `. .  \\ |       j . / ..'.,-\"\". \\ || ||   |  |\n"))
+    print((r"      `.        `.`. \\'.      | | | |||.   .,.','/ /'   |  |\n"))
+    print((r"        `-.       `. .:\\      | | | ||'`..___.'.','/   j   |\n"))
+    print((r"           `-._     `-._\\      \\'.`.`..`..__....','   ,   /\n"))
+    print((r"               `--......-\\      \\ `.`.`.......-\"'   ,'   /\n"))
+    print((r"                          `.     `. `-..____,.-\" _.'   ,'\n"))
+    print((r"                            >.     '--...___,..-'   _.\"\n"))
+    print((r"                          ,'  `--,__            _,-\"  `-.\n"))
+    print((r"                      _.-'      '   `'--------\"' `.      `-.\n"))
+    print((r"                    .'        ,'                   \\        `.\n"))
+    print((r"                   .        .'                      `.        .\n"))
+    print((r"                   |      ,'                          `._     |\n"))
+    print((r"                    `----'                               `\"--\" mh\n"))
+    print(("\n"))
+    print(("\n"))
+def level_2():
+    print((r"            ___   _,-'\"\"-.\n"))
+    print((r"          /`.  `./,\\`.    `.\n"))
+    print((r"         /'. |  / || |      .\n"))
+    print((r"        . `|,+-'| `| |       `._\n"))
+    print((r"        `,-'    `. | '          `.\n"))
+    print((r"       ,' -'      `\"'             `.\n"))
+    print((r"      /\"`-.                         `.\n"))
+    print((r"     :`.   \\                          .\n"))
+    print((r"    ' `.\\   \\                          `\n"))
+    print((r"   .`.  .`   `           `.             .\n"))
+    print((r"  / `.'  .`   .      .     \\             \\\n"))
+    print((r" /``  .`  `\\   .      \\     .             .\n"))
+    print((r"j  .\\  .:  .'  :       .    '             |\n"))
+    print((r"|`. .. ||  :|  |        ,..--`._          |\n"))
+    print((r"|:| |: :|  |:  |      ,'        |         |\n"))
+    print((r"||| :: |:  |:  |      |         |         |\n"))
+    print((r":'j '| :|  :|  |      |         |         |\n"))
+    print((r"`/ .j  ,:  :|  |     ,'         `         |\n"))
+    print((r" \\/ / . '  ',  |   .'            \\        '\n"))
+    print((r" ` , /,'  /.  j   |               .      /\n"))
+    print((r"  `.'/   /'   '   |  .             .   ,'\n"))
+    print((r"    .  .'/   /    `._/             '  /\n"))
+    print((r"     `.,'   /       |          __,' .'\n"))
+    print((r"       `-._,         `.   _,.-\" _,-'\n"))
+    print((r"           `+..____    `\"'    .'\n"))
+    print((r"          _/    |  `\"\"\"''\\    '\n"))
+    print((r"    _,.-\"'      \\        |     \\\n"))
+    print((r"_.-'             .       |      `\n"))
+    print((",                 |       |       `.\n"))
+    print(("`_          __,,-'        '         `.\n"))
+    print((r" `'\"\"'\"'\"''             .            .\n"))
+    print((r"                        |            |\n"))
+    print((r"                        '            /\n"))
+    print((r"                         `.        ,'\n"))
+    print((r"                           `-..,.-' mh\n"))
+def level_3():
+    print((r"                              __,.-\"\"\"'\"--..._.,---.\n"))
+    print((r"                          _,-'               /      `.\n"))
+    print((r"                       _.'                 ,'   ,-\"\"`.|\n"))
+    print((r"                     ,'                        / ,+\"`.;\n"))
+    print((r"                   ,'   ____                  . |_/  /\n"))
+    print((r"                  /  .\"'    `-.               ` `..-/`.  _\n"))
+    print((r"                 '  /                     .    `---'   `: `.\n"))
+    print((r"                /  .    ,-\"\"`.           .'             \\`-.`+\"\"`\"`.\n"))
+    print((r"               /   |  .' ,\"\".|               _,...,._    L |  `     `.\n"))
+    print((r"              /    |  | ._)  /     \\    _.-\"'        \"`. | j          .\n"))
+    print((r"             j     `  ' |  ,'        ,-'   ___......_   .|\"           |\n"))
+    print((r"             |         `-+'        ,'  _,\"__.---\"\"\"`-.`.||            |\n"))
+    print((r"             '                   ,'  ,'.-'    _______ `.`|\\           |\n"))
+    print((r"           .\"                   /  ,\",'   _,\"+.------+`.`:|           |\n"))
+    print((r"         ,'     .             ,' .,-'   .\".-'  _..._  ` \\''    .      ;\n"))
+    print((r"      _,+      /             ,  ,/    ,'.' .-\",.----.+ `7  `.   `._,.'\n"))
+    print((r"     /  '    ,'             .  '.   ,\".\" .:,-'__     :|j     `-.-'\n"))
+    print((r"    j    \\  /|`            ,  //   .,' ,'.' .\"__`.   ||'\n"))
+    print((r"  ,-+     `\" | \\             /.   //  /,\" ,'.'  \\ \\  |'\n"))
+    print((r" /           '  \\         : j '  //  //  .,'     || ,\"\n"))
+    print((r"/           /    \\        | :|  j.' j/   ||_.\") , ;<_\n"))
+    print((".            `-.   `.      : ||  || |.    `..-'.'.'   `'-._\n"))
+    print(("|              |     `.    | |:  :| |'        _,'          `.\n"))
+    print(("|              |      /`-._`.`:. \\' `.`..__.-'+              `\n"))
+    print(("`          .   F     ,     /\"`+-+.^+--`\"\"\"     `._            |\n"))
+    print((r"`.       |   /     /     .                       `._         /\n"))
+    print((r"  `._   _,..'    ,'      '                          `\"-....-'\n"))
+    print((r"     `\"\"        /       j\n"))
+    print((r"              ,'        |\n"))
+    print((r"             /          |\n"))
+    print((r"            j           '\n"))
+    print((r"            |          /\n"))
+    print((r"            |         /\n"))
+    print((r"            '        /\n"))
+    print((r"             `.___,.' mh\n"))
+
+def core_game():
+    global day
+    global level
+    global name
+    print("welcome to pokemon!")
+    while True:
+        print(f"it is day {day} and you have a level {level} {name}, he has leveled up due to training his dampness")
+        question = input("What would you like to do today?(train, rest, battle, or leave) ")
+        if question == "train":
+            day = day + 1
+            level = level + 1
+            if level >= 6 and name == "poliwag":
+                print("your poliwag has evolved into a poliwhirl")
+                level_2()
+                level = 1
+                name = "poliwhirl"
+            elif level >= 6 and name == "poliwhirl":
+                print("your poliwhirl has evolved into a poliwrath")
+                level_3()
+                level = 1
+                name = "poliwrath"
+            elif level >= 15:
+                print("you have reached the max level, do the final battle to complete the game")
+        elif question == "rest":
+            day = day + 1
+            print(f"It is day {day} and you have a level {level} {name}")
+        elif question == "leave":
+            print("thank you for playing!")
+            break
+        elif question == "battle":
+            bat = input("would you like to do a gym_battle or the final_boss_battle? ")
+            if bat == "gym_battle":
+                odds = random.randint(1,100)
+                if odds <= 70:
+                    print("congrats you have won a gym battle, your pokemon has leveled up 2 points")
+                    level = level + 2
+                if odds >= 70:
+                    print("sorry you have lost a gym battle, your pokemon has not leveled up or down")
+            if bat == "final_boss_battle":
+                odds = random.randint(1,100)
+                if level <= 5:
+                    if odds <= 10:
+                        print("congrats you have won the final boss battle and you have completed the game")
+                        break
+                    if odds >= 10:
+                        print("sorry you have lost the final boss battle and you have lost the game, please try again another time")
+                        break
+                if level <= 10:
+                    if odds <= 25:
+                        print("congrats you have won the final boss battle and you have completed the game")
+                        break
+                    if odds >= 25:
+                        print("sorry you have lost the final boss battle and you have lost the game, please try again another time")
+                        break
+                if level <= 15:
+                    if odds <= 50:
+                        print("congrats you have won the final boss battle and you have completed the game")
+                        break
+                    if odds >= 50:
+                        print("sorry you have lost the final boss battle and you have lost the game, please try again another time")
+                        break
+
+core_game()
+
